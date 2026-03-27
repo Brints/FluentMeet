b18620473620994ab65fbc6773f925fa938be26e
aniebietafia <aniebietafia87@gmail.com>
2026-03-27 23:28:28 +0100

    fix: Added sanitize logs helper to user input.
    Signed-off-by: aniebietafia <aniebietafia87@gmail.com>

--- alembic/versions/e1a664780dc6_add_meeting_model.py
+++ alembic/versions/e1a664780dc6_add_meeting_model.py
@@ -5,15 +5,15 @@ Revises: 19dc9714d9ea
 Create Date: 2026-03-27 22:18:35.070453
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
-revision: str = 'e1a664780dc6'
-down_revision: Union[str, Sequence[str], None] = '19dc9714d9ea'
+revision: str = "e1a664780dc6"
+down_revision: Union[str, Sequence[str], None] = "19dc9714d9ea"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
